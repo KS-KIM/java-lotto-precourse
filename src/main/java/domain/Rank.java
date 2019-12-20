d049036a1ad0426eb96ccbbcbb23598b5bcc069d
KSKIM <kwangsoonkim@outlook.com>
2019-12-20 20:31:06 +0900

    refactor: 로또의 승리와 관련된 Rank 코드 개선

--- src/main/java/domain/Rank.java
+++ src/main/java/domain/Rank.java
@@ -1,54 +1,61 @@
 package domain;
 
+import java.util.stream.Stream;
+
 /**
  * 로또 등수를 의미하는 enum
+ *
+ * @version 1.0.0
+ * @author KSKIM
+ * @since 2019-12-20
  */
 public enum Rank {
-    FIRST(6, 2_000_000_000), // 1등
-    SECOND(5, 30_000_000), // 2등
-    THIRD(5, 1_500_000), // 3등
-    FOURTH(4, 50_000), // 4등
-    FIFTH(3, 5_000), // 5등
-    MISS(0, 0);
+	FIRST(6, 2_000_000_000), // 1등
+	SECOND(5, 30_000_000), // 2등
+	THIRD(5, 1_500_000), // 3등
+	FOURTH(4, 50_000), // 4등
+	FIFTH(3, 5_000), // 5등
+	MISS(0, 0);
 
-    private static final int WINNING_MIN_COUNT = 3;
+	private static final int WINNING_MIN_COUNT = 3;
 
-    private int countOfMatch;
-    private int winningMoney;
+	private int countOfMatch;
+	private int winningMoney;
 
-    private Rank(int countOfMatch, int winningMoney) {
-        this.countOfMatch = countOfMatch;
-        this.winningMoney = winningMoney;
-    }
+	Rank(int countOfMatch, int winningMoney) {
+		this.countOfMatch = countOfMatch;
+		this.winningMoney = winningMoney;
+	}
 
-    public int getCountOfMatch() {
-        return countOfMatch;
-    }
+	public int getCountOfMatch() {
+		return countOfMatch;
+	}
 
-    public int getWinningMoney() {
-        return winningMoney;
-    }
+	public int getWinningMoney() {
+		return winningMoney;
+	}
 
-    public static Rank valueOf(int countOfMatch, boolean matchBonus) {
-        if (countOfMatch < WINNING_MIN_COUNT) {
-            return MISS;
-        }
+	public int calculateWinningMonies(int countOfMatch) {
+		return winningMoney * countOfMatch;
+	}
 
-        if (SECOND.matchCount(countOfMatch) && matchBonus) {
-            return SECOND;
-        }
+	public static Rank valueOf(int countOfMatch, boolean matchBonus) {
+		if (countOfMatch < WINNING_MIN_COUNT) {
+			return MISS;
+		}
 
-        for (Rank rank : values()) {
-            if (rank.matchCount(countOfMatch)) {
-                return rank;
-            }
-        }
+		if (SECOND.matchCount(countOfMatch) && matchBonus) {
+			return SECOND;
+		}
 
-        throw new IllegalArgumentException(countOfMatch + "는 유효하지 않은 값입니다.");
-    }
+		return Stream.of(Rank.values())
+				.filter(it -> it.matchCount(countOfMatch))
+				.findAny()
+				.orElseThrow(() -> new IllegalArgumentException(countOfMatch + "는 유효하지 않은 값입니다."));
+	}
 
-    private boolean matchCount(int countOfMatch) {
-        return this.countOfMatch == countOfMatch;
-    }
+	private boolean matchCount(int countOfMatch) {
+		return this.countOfMatch == countOfMatch;
+	}
 }
 
