d4a14f8607211da7efcffdb6fce431f913bb1b35
KSKIM <kwangsoonkim@outlook.com>
2019-12-20 20:24:42 +0900

    feat: 로또를 구입할 수 있는 금액을 의미하는 LottoPurchaseMoney 추가

---
+++ src/main/java/domain/lotto/LottoPurchaseMoney.java
@@ -0,0 +1,48 @@
+package domain.lotto;
+
+/**
+ * 로또 구입금액
+ *
+ * @version 1.0.0
+ * @author KSKIM
+ * @since 2019-12-20
+ */
+public class LottoPurchaseMoney {
+	private static final String MONEY_OUT_OF_RANGE_EXCEPTION = "잘못된 금액을 입력하셨습니다.";
+	private static final String INVALID_UNIT_EXCEPTION = "잘못된 금액을 입력하셨습니다.";
+	private static final int MIN_MONEY = 1_000;
+	private static final int MAX_MONEY = 50_000;
+	private static final int PURCHASE_UNIT = 1_000;
+
+	private final int money;
+
+	public LottoPurchaseMoney(int money) {
+		validateMoney(money);
+		this.money = money;
+	}
+
+	private void validateMoney(int money) {
+		validateRange(money);
+		validateUnit(money);
+	}
+
+	private void validateRange(int money) {
+		if (money < MIN_MONEY || money > MAX_MONEY) {
+			throw new IllegalArgumentException(MONEY_OUT_OF_RANGE_EXCEPTION);
+		}
+	}
+
+	private void validateUnit(int money) {
+		if (money % PURCHASE_UNIT != 0) {
+			throw new IllegalArgumentException(INVALID_UNIT_EXCEPTION);
+		}
+	}
+
+	public static LottoPurchaseMoney parse(String text) {
+		return new LottoPurchaseMoney(Integer.parseInt(text));
+	}
+
+	public int calculatePurchaseCount() {
+		return money / PURCHASE_UNIT;
+	}
+}
